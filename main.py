e6e27f022d58aeef4990b22805b2be06e31e267b
James Gebara <30349018+CrunchyFlannel@users.noreply.github.com>
2019-06-22 21:03:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import math
 #in python 'def' means "function" \/
 
 def chemTimer(currentHour, currentMinute, delta):
-	#the percent sign means mod() so in your case do mod(currentMinute + delta, 60) if this does not work try flipping the 60 and the currentMinute
+	'''the percent sign means mod() so in your case do mod(currentMinute + delta, 60) 
+	if this does not work try flipping the 60 and the currentMinute'''
 	minuteHand = (currentMinute + delta) % 60
 
 
@@ -22,8 +23,10 @@ def chemTimer(currentHour, currentMinute, delta):
 	print("Hour = " + str(hourHand))
 	print("Minute Hand = " + str(minuteHand))
 
-#This calls the function, try any inputs you would like it works everytime. Also the problem states you may have problems in matlab if the delta input is negative. If you do have problems just remember you can multiply the inputs by -1 to flip the sign.\/\/
-#inputs listed as follows: current hour, current minute, delta \/
+'''This calls the function, try any inputs you would like it works everytime. Also the problem states you may have problems 
+in matlab if the delta input is negative. If you do have problems just remember you can multiply the inputs by -1 to flip the sign.
+inputs listed as follows: current hour, current minute, delta \/'''
+
 chemTimer(6, 45, 10)
 
 #hopefully this code isnt to hard to translate back, I tried to explain it as best I could lol
